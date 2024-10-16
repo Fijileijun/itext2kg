1db72895bb935215f0372162ab797cef22d76421
jacobs <jacobs.lei@yumchina.com>
2024-10-16 09:35:44 +0800

    added about dishes domain schema

--- itext2kg/documents_distiller/__init__.py
+++ itext2kg/documents_distiller/__init__.py
@@ -1,10 +1,12 @@
+from build.lib.itext2kg.utils import DataHandler
 from .documents_distiller import DocumentsDistiller
-from ..utils.schemas import InformationRetriever, EntitiesExtractor, RelationshipsExtractor, Article, CV
+from ..utils.schemas import InformationRetriever, EntitiesExtractor, RelationshipsExtractor, Article, CV,Product
 __all__ = ["DocumentsDistiller",
            "DataHandler",
            "InformationRetriever", 
            "EntitiesExtractor", 
            "RelationshipsExtractor", 
            "Article", 
-           "CV"
+           "CV",
+           "Product"
            ]
